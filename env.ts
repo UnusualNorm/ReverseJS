ee55c7ea71c1ff863add3aee8d9c32d1e7c43cbb
Unusual norm <40722525+UnusualNorm@users.noreply.github.com>
2023-09-11 18:55:40 -0500

    Update env handling

--- env.ts
+++ env.ts
@@ -1,7 +1,8 @@
 // ----- FRESH ----- //
 // We techincally shouldn't toss undefined into parseInt, but it shouldn't crash the program
-export const PORT = parseInt(Deno.env.get("PORT")!);
-export const HOSTNAME = Deno.env.get("HOST")!;
+// @ts-expect-error - parseInt can take undefined
+export const PORT = parseInt(Deno.env.get("PORT")) || undefined;
+export const HOSTNAME = Deno.env.get("HOST");
 
 // ----- REVERSEJS ----- //
-export const PASSWORD = Deno.env.get("PASSWORD")!;
+export const PASSWORD = Deno.env.get("PASSWORD") ?? "password";
