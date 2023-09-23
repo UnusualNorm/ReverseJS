9745abb2030e186578ca54ef399ffbd023b7ca7c
Unusual norm <40722525+UnusualNorm@users.noreply.github.com>
2023-09-22 19:21:19 -0500

    Remove reference to old bundler solution

--- routes/api/bundle.ts
+++ routes/api/bundle.ts
@@ -3,7 +3,6 @@ import { PASTEBIN_API_KEY } from "../../env.ts";
 import type { HandlerContext } from "$fresh/server.ts";
 import type { GlobalState } from "../_middleware.ts";
 import { PasteClient } from "https://deno.land/x/deno_pastebin@0.0.1/mod.ts";
-import { modules } from "../modules/[id].ts";
 
 const client = new PasteClient(PASTEBIN_API_KEY);
 
@@ -21,9 +20,6 @@ export const handler = async (
   if (!userCode) {
     return new Response(null, { status: 400 });
   }
-
-  let id = Date.now();
-  while (modules.has(id.toString())) id++;
 
   let code: string;
   try {
